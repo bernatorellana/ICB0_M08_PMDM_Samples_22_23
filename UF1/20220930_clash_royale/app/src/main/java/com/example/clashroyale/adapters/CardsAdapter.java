f88c7a19e51bbafe834d5772d5ed3c557e1a958f
bernat <bernat.orellana@gmail.com>
2022-10-05 18:39:41 +0200

    s

--- UF1/20220930_clash_royale/app/src/main/java/com/example/clashroyale/adapters/CardsAdapter.java
+++ UF1/20220930_clash_royale/app/src/main/java/com/example/clashroyale/adapters/CardsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.clashroyale.adapters;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,7 @@ import com.example.clashroyale.model.Card;
 
 import java.util.List;
 
-public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> {
+public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder>  {
 
     List<Card> mCards;
     public CardsAdapter(List<Card> pCards){
@@ -25,16 +26,36 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View fila = LayoutInflater.from(parent.getContext()).inflate(R.layout.fila, parent, false);
-        return new ViewHolder(fila);
+        ViewHolder vh = new ViewHolder(fila);
+
+        // Programació del clic de la fila
+        fila.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int pos = vh.getAdapterPosition();
+                Log.d("XXX", "Han fet click a la posició "+pos);
+                //mCards.get(pos).setSelected(true);
+                mCards.get(pos).toogleSelect();
+                notifyDataSetChanged();
+            }
+        });
+
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Card c = mCards.get(position);
-        holder.txvName.setText( c.getId()+"-"+ c.getName());
+        holder.txvName.setText( c.getId()+"-"+ c.getName()+">"+(c.isSelected()?"S":"N"));
         holder.txvDesc.setText(c.getDesc());
         holder.txvCost.setText(""+c.getElixirCost());
         holder.imvPhoto.setImageResource(c.getDrawable());
+        Log.d("XXX", "Actualitzant fila "+position);
+        holder.vieSelected.setVisibility( c.isSelected()?
+                                            View.VISIBLE:
+                                            View.INVISIBLE );
+
     }
 
     @Override
@@ -42,17 +63,20 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
         return mCards.size();
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView txvName;
         TextView txvCost;
         TextView txvDesc;
         ImageView imvPhoto;
+        View vieSelected;
         public ViewHolder(@NonNull View fila) {
             super(fila);
             txvName = fila.findViewById(R.id.txvName);
             txvCost = fila.findViewById(R.id.txvCost);
             txvDesc = fila.findViewById(R.id.txvDesc);
             imvPhoto = fila.findViewById(R.id.imvPhoto);
+            vieSelected = fila.findViewById(R.id.vieSelected);
         }
     }
 }
